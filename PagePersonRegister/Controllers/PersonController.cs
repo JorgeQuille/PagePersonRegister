684b49debe10e3b9912bc933a5a37d46a3347214
Jorge Quille <44824514+JorgeQuille@users.noreply.github.com>
2022-08-29 08:28:49 -0500

    Agregar archivos de proyecto.

---
+++ PagePersonRegister/Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using PagePersonRegister.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PagePersonRegister.Controllers
+{
+    public class PersonController : Controller
+    {
+        private PersonDBEntities1 db = new PersonDBEntities1();
+
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View(db.Person.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(person);
+        }
+        public ActionResult Edit(int id)
+        {
+            Person person = db.Person.Find(id);
+            return View(person);
+        }
+        [HttpPost]
+        public ActionResult Edit(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+    }
+}
---
+++ PagePersonRegister/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PagePersonRegister.MvcApplication" Language="C#" %>
